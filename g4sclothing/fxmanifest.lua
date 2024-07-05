f412012cc46d0aa3810112a83c09db152c2a7bbf
Cloud Gaming <149394072+CloudGamingServices@users.noreply.github.com>
2024-07-05 19:45:22 +0100

    G4S Vest

---
+++ g4sclothing/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+authour {'trapmansosa'}
+files {
+  'mp_m_freemode_01_mp_m_g4s.meta',
+  'mp_f_freemode_01_mp_f_g4s.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_g4s.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_g4s.meta'
